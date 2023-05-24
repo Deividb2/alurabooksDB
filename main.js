3a4e6547b94c2ac2cec577456b62f56b192d15b1
Deivid Barreto <deividbarreto57@gmail.com>
2023-05-23 22:49:09 -0300

    Adicionando carrossel com Swiper

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ const menuNav = document.querySelector('.menu-nav');
 btnMenu.addEventListener('click', () => {
     menuNav.classList.toggle('menu-navAtivo');
 })
+
+const swiper = new Swiper('.swiper', {
+    spaceBetween: 10,
+    slidesPerView: 3,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+});
