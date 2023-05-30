e22522e91c5fa6ea51abf5d78b1852c69983f490
Deivid Barreto <deividbarreto57@gmail.com>
2023-05-29 23:31:42 -0300

    atualização

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const btnMenu = document.querySelector('.cabecalho__menu-hamburguer');
 const menuNav = document.querySelector('.menu-nav');
+const img = document.querySelector('.container__imagem')
 btnMenu.addEventListener('click', () => {
     menuNav.classList.toggle('menu-navAtivo');
-})
+    if (menuNav.classList.contains('menu-navAtivo')) {
+        
+    }
+});
 
 const swiper = new Swiper('.swiper', {
     spaceBetween: 10,
